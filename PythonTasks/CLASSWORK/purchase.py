0cc72f0af1fe8384d60acd9fa07b8b224ee3eca4
Seunfunmi-ovie <oluwaseunpu@gmail.com>
2026-04-17 14:43:48 +0100

    purchase.py

--- PythonTasks/CLASSWORK/purchase.py
+++ PythonTasks/CLASSWORK/purchase.py
@@ -1,4 +1,4 @@
-#start by entering the total spending in the store
+#start by entering the total spending in the stop
 # if the purchase is between 1000 and 10000 then the customer is to have a 5% discount
 #else if the purchase is 10000 and 500000 then the customer get 10% discount
 # else if above 50000 receive a 20 discount
@@ -13,7 +13,7 @@ if customer <= 10000:
 elif customer <= 50000:
      discount = customer * 0.10
      print("Discount: ", discount)
-elif customer <= 5000:
+elif customer >= 50000:
      discount = customer * 0.20
      print("Discount: ", discount)
 
