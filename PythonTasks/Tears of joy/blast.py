6bae59a863da53e24accb8b75a4f48f9ce5cb103
Seunfunmi-ovie <oluwaseunpu@gmail.com>
2026-04-21 23:40:02 +0100

    Tears of joy

---
+++ PythonTasks/Tears of joy/blast.py
@@ -0,0 +1,6 @@
+for number in range (10, 0,-1):
+    if number ==1:
+       print(number, "Blast off!!!!")
+
+    else:
+       print(number)
---
+++ PythonTasks/Tears of joy/name.py
@@ -0,0 +1,3 @@
+name = input ("what is your name: ")
+for number in range (5):
+    print("Hello",name)
