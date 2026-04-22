6bea3fb1f1a42025e6bf73464db96fc94047585e
Seunfunmi-ovie <oluwaseunpu@gmail.com>
2026-04-22 15:56:48 +0100

    Tears of joy

--- PythonTasks/Tears of joy/largest.py
+++ PythonTasks/Tears of joy/largest.py
@@ -1,6 +1,12 @@
-number = input("Enter the number: ")
-number = int(number)
-largest = 0 
-for i in range (10):
-    if number > largest:
-largest = number
+def largest(number_one, number_two, number_three):
+    largest = number_one
+    if number_two > largest:
+        largest = number_two
+    if number_three > largest:
+        largest = number_three
+    return largest
+print (largest(number_three = 50, number_two = 60, number_one = 20))
+
+# using the keyword argument we give name to the variable so it is easily readable
+# using positional argument means not defining the actually value to the variable
+
